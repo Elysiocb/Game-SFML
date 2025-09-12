f7700de1151bced208cd5a8c25fe7075c503bb14
Elysiocb <aldo.elysio@gmail.com>
2025-09-12 01:58:54 -0300

    background adicionado

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,41 +3,57 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 800u, 600u }), "Exemplo SFML");
+    sf::RenderWindow window(sf::VideoMode({800u, 600u}), "Exemplo SFML");
+    window.setFramerateLimit(60);
 
-    sf::Texture texture;
-    if (!texture.loadFromFile("foto1.jpeg"))
+    // Carregando Texturas
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("background.png"))
         return -1;
 
-    sf::Sprite sprite(texture);
+    sf::Texture button;
+    if (!button.loadFromFile("button.png"))
+        return -1;
 
-    // Centralizar a imagem
-    sprite.setOrigin(sf::Vector2f(texture.getSize().x / 2.f, texture.getSize().y / 2.f));
-    sprite.setPosition(sf::Vector2f(800.f / 2.f, 600.f / 2.f));
+    sf::Sprite backgroundSprite(backgroundTexture);
+    /*
+    backgroundSprite.setOrigin(
+        sf::Vector2f(backgroundTexture.getSize().x / 2.f,
+                     backgroundTexture.getSize().y / 2.f)
+    );*/
 
-    // Display the list of all the video modes available for fullscreen
+    /* Mostrar modos fullscreen disponíveis
     std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
     for (std::size_t i = 0; i < modes.size(); ++i)
     {
         sf::VideoMode mode = modes[i];
         std::cout << "Mode #" << i << ": "
-            << mode.size.x << "x" << mode.size.y << " - "
-            << mode.bitsPerPixel << " bpp" << std::endl;
-    }
+                  << mode.size.x << "x" << mode.size.y << " - "
+                  << mode.bitsPerPixel << " bpp" << std::endl;
+    }*/
 
-    // Create a window with the same pixel depth as the desktop
+    // Criar janela no tamanho desejado
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    window.create(sf::VideoMode({ 1024, 768 }, desktop.bitsPerPixel), "SFML window");
+    window.create(sf::VideoMode({1024u, 768u}, desktop.bitsPerPixel), "Windows XP Simulator");
 
+	//Redimensionar a textura
+    backgroundSprite.setScale(
+        sf::Vector2f(
+            static_cast<float>(window.getSize().x) / backgroundTexture.getSize().x,
+            static_cast<float>(window.getSize().y) / backgroundTexture.getSize().y
+        )
+    );
+    backgroundSprite.setPosition({ 0.f,0.f });
+
+    // Loop principal
     while (window.isOpen())
     {
-        // Processar eventos para manter a janela responsiva
         while (auto event = window.pollEvent())
         {
-            if (event->is<sf::Event::Closed>())        // Fecha ao clicar no X
+            if (event->is<sf::Event::Closed>())
                 window.close();
 
-            if (event->is<sf::Event::KeyPressed>())   // Fecha ao apertar Esc
+            if (event->is<sf::Event::KeyPressed>())
             {
                 const auto& keyEvent = event->getIf<sf::Event::KeyPressed>();
                 if (keyEvent && keyEvent->code == sf::Keyboard::Key::Escape)
@@ -46,7 +62,7 @@ int main()
         }
 
         window.clear(sf::Color::Black);
-        window.draw(sprite);
+        window.draw(backgroundSprite);
         window.display();
     }
 
